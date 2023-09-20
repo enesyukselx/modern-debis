796973c8de57be3a513a627c0bac7a03685d86a1
Enes Yüksel <enesyuksel9903@gmail.com>
2023-09-20 15:45:42 +0300

    Create SelectSemester.tsx

---
+++ app/(dashboard)/dashboard/results/components/SelectSemester.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+const SelectSemester = ({ semesters }) => {
+    const router = useRouter();
+    return (
+        <select
+            name=""
+            id=""
+            className="block w-full mt-4 rounded-md bg-gray-200 border-transparent focus:border-gray-500 "
+            defaultValue={0}
+            onChange={(e) => {
+                router.push("/dashboard/results/" + e.target.value);
+            }}
+        >
+            <option value="0" disabled>
+                Dönem Seçiniz.
+            </option>
+            {semesters.map((item, index) => {
+                return (
+                    <option key={index} value={item.value}>
+                        {item.text}
+                    </option>
+                );
+            })}
+        </select>
+    );
+};
+
+export default SelectSemester;
