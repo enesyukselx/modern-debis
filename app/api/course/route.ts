734611d023eefee0eda74601718ce40efbe4be4c
Enes Yüksel <enesyuksel9903@gmail.com>
2023-09-22 15:13:44 +0300

    Update route.ts

--- app/api/course/route.ts
+++ app/api/course/route.ts
@@ -23,75 +23,69 @@ export async function GET(request: NextRequest) {
         );
     }
 
-    try {
-        const courseData = qs.stringify({
-            ogretim_donemi_id: semester,
-            ders: course,
-        });
+    const courseData = qs.stringify({
+        ogretim_donemi_id: semester,
+        ders: course,
+    });
 
-        const response: any = await debisApi(
-            "POST",
-            "OgrenciIsleri/Ogrenci/OgrenciNotu/index.php",
-            { Cookie: `PHPSESSID=${session}` },
-            courseData
-        );
+    const response: any = await debisApi(
+        "POST",
+        "OgrenciIsleri/Ogrenci/OgrenciNotu/index.php",
+        { Cookie: `PHPSESSID=${session}` },
+        courseData
+    );
 
-        const $ = cheerio.load(response.iconv, {
-            decodeEntities: false,
-        });
+    const $ = cheerio.load(response.iconv, {
+        decodeEntities: false,
+    });
 
-        const table = $(
-            'table[style="margin-top:10px"] > tbody > tr > td > table'
-        );
+    const table = $('table[style="margin-top:10px"] > tbody > tr > td > table');
 
-        const td = table.find("td");
-        const courseDetails: string[] = [];
-        for (let i = 0; i < td.length; i++) {
-            courseDetails.push(td.eq(i).text());
+    const td = table.find("td");
+    const courseDetails: string[] = [];
+    for (let i = 0; i < td.length; i++) {
+        courseDetails.push(td.eq(i).text());
+    }
+
+    const courseArr: ICourseArr = {
+        credit: courseDetails[11],
+        type: courseDetails[14],
+        professor: courseDetails[20],
+        status: courseDetails[23],
+        results: [],
+    };
+
+    courseDetails.forEach((element, index) => {
+        if (
+            element == "Vize" ||
+            element == "Final" ||
+            element == "Bütünleme Notu" ||
+            element == "Ödev" ||
+            element == "Quiz" ||
+            element == "Vize / Ödev"
+        ) {
+            courseArr.results?.push({
+                name: courseDetails[index],
+                avarage: courseDetails[index + 2],
+                result: courseDetails[index + 4],
+            });
         }
 
-        const courseArr: ICourseArr = {
-            credit: courseDetails[11],
-            type: courseDetails[14],
-            professor: courseDetails[20],
-            status: courseDetails[23],
-            results: [],
-        };
+        if (
+            element == "Yarıyıl Sonu Başarı Notu" ||
+            element == "Bütünleme Sonu Başarı Notu" ||
+            element == "Başarı Notu"
+        ) {
+            courseArr.results?.push({
+                name: courseDetails[index],
+                result: courseDetails[index + 4],
+            });
+        }
+    });
 
-        courseDetails.forEach((element, index) => {
-            if (
-                element == "Vize" ||
-                element == "Final" ||
-                element == "Bütünleme Notu" ||
-                element == "Ödev" ||
-                element == "Quiz" ||
-                element == "Vize / Ödev"
-            ) {
-                courseArr.results?.push({
-                    name: courseDetails[index],
-                    avarage: courseDetails[index + 2],
-                    result: courseDetails[index + 4],
-                });
-            }
-
-            if (
-                element == "Yarıyıl Sonu Başarı Notu" ||
-                element == "Bütünleme Sonu Başarı Notu" ||
-                element == "Başarı Notu"
-            ) {
-                courseArr.results?.push({
-                    name: courseDetails[index],
-                    result: courseDetails[index + 4],
-                });
-            }
-        });
-
-        return new Response(
-            JSON.stringify({
-                courseArr,
-            })
-        );
-    } catch (error) {
-        console.error(error);
-    }
+    return new Response(
+        JSON.stringify({
+            courseArr,
+        })
+    );
 }
