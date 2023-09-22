c741758465d2b5ce5c9e000b18e9828812b53720
Enes Yüksel <enesyuksel9903@gmail.com>
2023-09-22 14:08:59 +0300

    debisApi function usage for all axios.get methods

--- app/api/schedule/route.ts
+++ app/api/schedule/route.ts
@@ -1,7 +1,7 @@
-import axios, { AxiosResponse } from "axios";
 import { NextRequest } from "next/server";
 import cheerio from "cheerio";
 import { ISchedule, TSession } from "@/app/types/api-types";
+import debisApi from "@/app/utils/api/debisApi";
 
 export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url);
@@ -14,48 +14,41 @@ export async function GET(request: NextRequest) {
             })
         );
     }
-    try {
-        const response: AxiosResponse = await axios.get(
-            "https://debis.deu.edu.tr/OgrenciIsleri/Ogrenci/DersProgrami/index.php",
-            {
-                headers: {
-                    Cookie: "PHPSESSID=" + session,
-                    Host: "debis.deu.edu.tr",
-                },
-            }
-        );
+    const response: any = await debisApi(
+        "GET",
+        "OgrenciIsleri/Ogrenci/DersProgrami/index.php",
+        {
+            sessionId: session,
+        }
+    );
 
-        const $ = cheerio.load(response.data);
+    const $ = cheerio.load(response.data);
 
-        const courses: ISchedule[] = [];
+    const courses: ISchedule[] = [];
 
-        const table = $(
-            'table[width="99%"][border="1"][cellspacing="0"][cellpadding="0"][align="center"][bordercolor="#FF9900"]'
-        );
-        const tr = table.find("tr");
+    const table = $(
+        'table[width="99%"][border="1"][cellspacing="0"][cellpadding="0"][align="center"][bordercolor="#FF9900"]'
+    );
+    const tr = table.find("tr");
 
-        tr.each((i, element) => {
-            const td = $(element).find("td");
-            const course = {
-                time: td.eq(0).text(),
-                monday: td.eq(1).text(),
-                tuesday: td.eq(2).text(),
-                wednesday: td.eq(3).text(),
-                thursday: td.eq(4).text(),
-                friday: td.eq(5).text(),
-            };
-            courses.push(course);
-        });
+    tr.each((i, element) => {
+        const td = $(element).find("td");
+        const course = {
+            time: td.eq(0).text(),
+            monday: td.eq(1).text(),
+            tuesday: td.eq(2).text(),
+            wednesday: td.eq(3).text(),
+            thursday: td.eq(4).text(),
+            friday: td.eq(5).text(),
+        };
+        courses.push(course);
+    });
 
-        courses.shift();
+    courses.shift();
 
-        return new Response(
-            JSON.stringify({
-                courses,
-            })
-        );
-    } catch (error) {
-        console.error(error);
-        return;
-    }
+    return new Response(
+        JSON.stringify({
+            courses,
+        })
+    );
 }
--- app/api/semesters/route.ts
+++ app/api/semesters/route.ts
@@ -1,8 +1,7 @@
-import axios, { AxiosResponse } from "axios";
 import { NextRequest } from "next/server";
 import cheerio from "cheerio";
-import iconv from "iconv-lite";
-import { IConfig, TSession } from "@/app/types/api-types";
+import { TSession } from "@/app/types/api-types";
+import debisApi from "@/app/utils/api/debisApi";
 
 export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url);
@@ -16,42 +15,31 @@ export async function GET(request: NextRequest) {
         );
     }
 
-    try {
-        const config: IConfig = {
-            headers: {
-                Cookie: "PHPSESSID=" + session,
-                Host: "debis.deu.edu.tr",
-            },
-            responseType: "arraybuffer",
+    const response: any = await debisApi(
+        "GET",
+        "OgrenciIsleri/Ogrenci/OgrenciNotu/index.php",
+        {
+            sessionId: session,
+        }
+    );
+
+    const $ = cheerio.load(response.data, {
+        decodeEntities: false,
+    });
+    const options = $("#ogretim_donemi_id").find("option");
+    const optionsArray: { value?: string; text?: string }[] = [];
+    options.each((i, element) => {
+        const option = {
+            value: $(element).attr("value"),
+            text: $(element).text(),
         };
+        optionsArray.push(option);
+    });
+    optionsArray.shift();
 
-        const response: AxiosResponse = await axios.get(
-            "https://debis.deu.edu.tr/OgrenciIsleri/Ogrenci/OgrenciNotu/index.php",
-            config
-        );
-
-        const decodedBody = iconv.decode(response.data, "ISO-8859-9"); // Convert Latin1 to UTF-8
-        const $ = cheerio.load(decodedBody, {
-            decodeEntities: false,
-        });
-        const options = $("#ogretim_donemi_id").find("option");
-        const optionsArray: { value?: string; text?: string }[] = [];
-        options.each((i, element) => {
-            const option = {
-                value: $(element).attr("value"),
-                text: $(element).text(),
-            };
-            optionsArray.push(option);
-        });
-        optionsArray.shift();
-
-        return new Response(
-            JSON.stringify({
-                semesters: optionsArray,
-            })
-        );
-    } catch (error) {
-        console.error(error);
-        return;
-    }
+    return new Response(
+        JSON.stringify({
+            semesters: optionsArray,
+        })
+    );
 }
